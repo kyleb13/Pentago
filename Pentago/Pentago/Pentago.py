1e1334d04078f34e3fe60c234888a43c897284a5
kyle beveridge <kbev4201@live.com>
2018-05-03 16:55:04 -0700

    Game features excluding main loop done

--- Pentago/Pentago/Pentago.py
+++ Pentago/Pentago/Pentago.py
@@ -3,6 +3,7 @@
 class Pentago:
     def __init__(self):
         self.board = [["-" for x in range(6)] for y in range(6)]
+        self.winner = ""
         self.gameLoop()
 
     def place(self, player, location = (0, 0)):
@@ -60,16 +61,82 @@ class Pentago:
     def gameOver(self):
         wcnt = 0
         bcnt = 0
+        for a in range(0, 6):#check rows for 5 in a row
+            wcnt = 0
+            bcnt = 0
+            for b in range(0, 6):
+                char = self.board[a][b]
+                wcnt = wcnt+1 if char == "w" else 0
+                bcnt = bcnt+1 if char == "b" else 0
+                if wcnt == 5 or bcnt == 5:
+                    self.winner = "w" if wcnt == 5 else "b"
+                    return True
+        for d in range(0, 6):#check columns for 5 in a row
+            wcnt = 0
+            bcnt = 0
+            for c in range(0, 6):
+                char = self.board[c][d]
+                wcnt = wcnt+1 if char == "w" else 0
+                bcnt = bcnt+1 if char == "b" else 0
+                if wcnt == 5 or bcnt == 5:
+                    self.winner = "w" if wcnt == 5 else "b"
+                    return True
+        for e in range(-1, 2):#check diagonals (left to right) for 5 in a row
+            wcnt = 0
+            bcnt = 0
+            for f in range(0, 6):
+                char = self.board[f][f]
+                if e==-1:
+                    char = self.board[f+1][f]
+                elif e == 1:
+                    char = self.board[f][f+1]
+                wcnt = wcnt+1 if char == "w" else 0
+                bcnt = bcnt+1 if char == "b" else 0
+                if wcnt == 5 or bcnt == 5:
+                    self.winner = "w" if wcnt == 5 else "b"
+                    return True
+                if e!=0 and f==4:
+                    break
+        for e in range(-1, 2):#check diagonals (right to left) for 5 in a row
+            wcnt = 0
+            bcnt = 0
+            for f in range(5, -1, -1):
+                char = self.board[5 - f][f]
+                if e==-1:
+                    char = self.board[6-f][f]
+                elif e == 1:
+                    char = self.board[5-f][f-1]
+                wcnt = wcnt+1 if char == "w" else 0
+                bcnt = bcnt+1 if char == "b" else 0
+                if wcnt == 5 or bcnt == 5:
+                    self.winner = "w" if wcnt == 5 else "b"
+                    return True
+                if e!=0 and f==1:
+                    break
+        return False
+
 
     
 
     def gameLoop(self):
-        
+        self.place("w", (0,5))
+        self.place("w", (1,4))
+        self.place("w", (2,3))
+        self.place("w", (3,2))
+        self.place("w", (4,1))
+        print(self.gameOver())
     
+
+def test():
+    for a in range(2):
+        for b in range(2):
+            print("inner loop")
+            if True:
+                break
+        print("Outer Loop")
 
 if __name__ == "__main__":
     game = Pentago()
 
-def test(val = []):
-    val[0] = 1
+
 
